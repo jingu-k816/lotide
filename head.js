2cc1985eb6479e1dd081e7459d54b62dbfbe3a12
Jingu Kang <jinguk0816@gmail.com>
2021-03-29 17:43:46 -0400

    a function to return the first element of the array. also returns undefined if the array is empty.

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const head = function(arr){
+  if(arr.length === 0){
+    return undefined;
+  }
+
+  return arr[0];
+};
+
+//console.log(head([5,6,7]));
+//Test
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
