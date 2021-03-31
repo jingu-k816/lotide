1322cb9190ab4585aa61d4222c033b34c58af9b4
Jingu Kang <jinguk0816@gmail.com>
2021-03-31 00:52:12 -0400

    function to take away the nested array in an array then replace with the element of an array

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${firstArray} != ${secondArray}`);
+  }
+};
+
+const flatten = function (elementsOfNestedArray){
+  const flattenedArray = [];
+  for(let i = 0; i < elementsOfNestedArray.length; i++){
+    const currentElement = elementsOfNestedArray[i];
+    if(!Array.isArray(currentElement)){
+      flattenedArray.push(currentElement);
+    }
+    for(let j = 0; j < currentElement.length; j++){
+      flattenedArray.push(currentElement[j]);
+    }
+  }
+  console.log(flattenedArray);
+}
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
