6f5acc5f3d5432c1d0dc0f8ddb2e20c336c84d08
Jingu Kang <jinguk0816@gmail.com>
2021-04-05 10:03:54 -0400

    removed the redundant functions and added require() to just bring the function from another file

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,11 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("jingu", "jingu");
-assertEqual("Jingu", "JINGU");
-assertEqual(1, 3);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("jingu", "jingu");
+// assertEqual("Jingu", "JINGU");
+// assertEqual(1, 3);
+
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} != ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr.length === 0) {
@@ -16,6 +10,8 @@ const head = function(arr) {
 
 //console.log(head([5,6,7]));
 //Test
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
+
+module.exports= head;
