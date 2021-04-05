ace91d4cd7c3a0696615b72aaaa779e06cec03ea
Jingu Kang <jinguk0816@gmail.com>
2021-04-05 16:21:35 -0400

    implemented mocha and chai testing into tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,33 @@
+const assert = require("chai").assert;
 const tail = require("../tail");
-const assertEqual = require('../assertEqual');
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-const results = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(results.length, 2); // ensure we get back two elements
-assertEqual(results[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(results[1], "Labs"); // ensure second element is "Labs"
 
 
-// // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('Tail Function', () => {
+  it("returns 2 for checking the length of ['Hello', 'Lighthouse' , 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]).length;
+    assert.deepEqual(result, 2);
+  });
+
+  it("returns 'Lighthouse' for checking the first index of ['Hello', 'Lighthouse' , 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result[0], 'Lighthouse');
+  });
+
+  it("returns 'Labs' for checking the last index of ['Hello', 'Lighthouse' , 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result[1], 'Labs');
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse' , 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ['Lighthouse', 'Labs']);
+  });
+});
+
+// assertEqual(results[1], "Labs"); // ensure second element is "Labs"
+
+
+// // // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
