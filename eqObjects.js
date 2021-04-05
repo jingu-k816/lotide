2bc7e85bd51c000612228e9ff6a7cd05386fa4a8
Jingu Kang <jinguk0816@gmail.com>
2021-04-05 16:58:50 -0400

    got rid of redundant functions from other files instead, called the required function from other file to implement eqObjects. Also exported oqObjects so this can be used in another file

--- eqObjects.js
+++ eqObjects.js
@@ -1,23 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} != ${expected}`);
-  }
-};
-
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function (object1, object2){
   const keysOfObject1 = Object.keys(object1);
@@ -39,37 +20,5 @@ const eqObjects = function (object1, object2){
   }
   return true;
 };
-//if (eqArrays(firstArray, secondArray))
-const ab = {
-  a: '1',
-  b: '2'
-};
-const ba = { 
-  b: '2',
-  a: '1'
-};
 
-const abc = {
-  a: "1",
-  b: "2",
-  c: "3"
-};
-//console.log(eqObjects(ab, ba));
-// assertEqual(eqObjects(ab, ba), true);
-// assertEqual(eqObjects(ab,abc), false);
-
-const cd = { 
-  c: "1",
-  d: ["2", 3] 
-};
-const dc = { 
-  d: ["2", 3], 
-  c: "1" 
-};
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { 
-  c: "1", 
-  d: ["2", 3, 4] 
-};
-assertEqual(eqObjects(cd, cd2), false); // => false
+module.exports = eqObjects;
