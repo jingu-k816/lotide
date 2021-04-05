83c012bc3c5afd2f0ab91cd351e1a9b484955f77
Jingu Kang <jinguk0816@gmail.com>
2021-04-05 17:00:24 -0400

    instead of having to bring functions from other file and declaring it every time, brought a file to implement assertObjectEqual and also exported the function so that it is accessible from other files

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,36 +1,5 @@
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const eqObjects = function (object1, object2){
-  const keysOfObject1 = Object.keys(object1);
-  const keysOfObject2 = Object.keys(object2);
-
-  if(keysOfObject1.length !== keysOfObject2.length){
-    return false;
-  }
-  for(const keyName of keysOfObject1){
-    if(Array.isArray(object1[keyName]) && Array.isArray(object2[keyName])){
-      if(!eqArrays(object1[keyName], object2[keyName])){
-        return false;
-      }
-    }else {
-      if(object1[keyName] !== object2[keyName]){
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected){
   const inspect = require('util').inspect;
@@ -42,17 +11,4 @@ const assertObjectsEqual = function(actual, expected){
   }
 };
 
-const cd = { 
-  c: "1",
-  d: ["2", 3] 
-};
-const dc = { 
-  d: ["2", 3], 
-  c: "1" 
-};
-const cd2 = { 
-  c: "1", 
-  d: ["2", 3, 4] 
-};
-
-assertObjectsEqual(cd, cd2);
+module.exports = assertObjectsEqual;
