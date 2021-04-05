faa36afedd42fe6e34fb32485c3c9e57a69dc095
Jingu Kang <jinguk0816@gmail.com>
2021-04-05 16:33:32 -0400

    implemented mocha and chai testing to middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,24 @@
+const assert = require('chai').assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-//TEST CODE
+describe('#Middle', () => {
+  it('returns empty array for [1]', ()=>{
+    assert.deepEqual(middle([1]), []);
+  });
 
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3, 4]); // => [2, 3]
-//console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+  it('returns empty array for [1, 2]', ()=>{
+    assert.deepEqual(middle([1,2 ]), []);
+  });
 
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
+  it('returns [2, 3] for [1, 2, 3, 4]', ()=>{
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('returns [3, 4] for [1, 2, 3, 4, 5, 6]', ()=>{
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it('returns [3] for [1, 2, 3, 4, 5]', ()=>{
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
