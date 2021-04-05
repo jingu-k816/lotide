52500599d11b09ea8a1677dd0cfbb14a45bcf136
Jingu Kang <jinguk0816@gmail.com>
2021-04-05 16:21:08 -0400

    cleaned out the unnecessary comments

--- tail.js
+++ tail.js
@@ -1,19 +1,3 @@
-const assertEqual = require('./assertEqual');
-
-// const tail = function(arr) {
-//   const newArr = [];
-//   if (arr.length <= 1) {
-//     return newArr;
-//   }
-//   for (let i = 1; i < arr.length; i++) {
-//     newArr.push(arr[i]);
-//   }
-//   return newArr;
-// };
-
-
-//use slice method (was given as a stretch goal from a peer assistant)
-
 const tail = function(arr){
   const newArr = arr.slice(1);
 
